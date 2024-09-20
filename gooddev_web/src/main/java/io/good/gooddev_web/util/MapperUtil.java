aecc8c644ea4b73e6e062bf23b517a4cdf617efd
MincheolSin <smc970417@gmail.com>
2024-09-20 13:47:12 +0900

    gooddev_web ver0.0.1 .git/COMMIT_EDITMSG[+] [unix] (13:47 20/09/2024)                        2,28 Top 14 more lines     </logger>
        <logger name="com.zaxxer.hikari.HikariConfig">
            <level value="debug" />
        </logger>
    
        <logger name="com.zaxxer.hikari.HikariDataSource">
            <level value="debug" />
        </logger>
    
        <logger name="com.zaxxer.hikari.pool">
            <level value="debug" />
        </logger>

---
+++ gooddev_web/src/main/java/io/good/gooddev_web/util/MapperUtil.java
@@ -0,0 +1,16 @@
+package io.good.gooddev_web.util;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapperUtil extends ModelMapper {
+	MapperUtil(){
+		this.getConfiguration()
+			.setFieldAccessLevel(AccessLevel.PRIVATE)
+			.setFieldMatchingEnabled(true)
+			.setMatchingStrategy(MatchingStrategies.STRICT);
+	}
+}
